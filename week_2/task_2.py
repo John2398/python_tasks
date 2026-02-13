0ff5c87749131927717c0894eabc9e7ce1b9c459
John2398 <oluwadarafabunmi@yahoo.com>
2026-02-13 20:54:52 +0100

    correction made to task_2

--- week_2/task_2.py
+++ week_2/task_2.py
@@ -1,9 +1,9 @@
-for a in range(100):
-    if a % 3:
+for a in range(101):
+    if a % 3 == 0:
         print("Fizz")
-    elif a % 5:
+    elif a % 5 == 0:
         print("Buzz")
-    elif (a % 3) & (a % 5):
+    elif (a % 3) == 0 & (a % 5) == 0:
         print("FizzBuzz")
     else:
         print("Not a multiple")
